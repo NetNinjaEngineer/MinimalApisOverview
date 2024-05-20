600bc6d7923e11eaa0bd22f2630ec385ce3fdca8
NetNinjaEngineer <memo5260287@outlook.com>
2024-05-20 22:28:26 +0300

    implemented todo endpoints

--- MinimalApisOverview/Program.cs
+++ MinimalApisOverview/Program.cs
@@ -6,7 +6,50 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
+app.MapGet("/todoitems", async (ApplicationDbContext db) =>
+    await db.Todos.ToListAsync());
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todoitems/complete", async (ApplicationDbContext db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, ApplicationDbContext db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, ApplicationDbContext db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, ApplicationDbContext db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, ApplicationDbContext db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
