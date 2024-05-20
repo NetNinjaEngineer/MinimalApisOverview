60cdab713e1a199729040a03325569ddc23997ae
NetNinjaEngineer <memo5260287@outlook.com>
2024-05-20 22:24:37 +0300

    added Todo model and context

---
+++ MinimalApisOverview/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApisOverview;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+}
--- MinimalApisOverview/Program.cs
+++ MinimalApisOverview/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApisOverview;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
